5a362c1e74e20d630d45d1aa1a2b853d78354903
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-01 09:19:42 -0700

    lynt issues?

--- .flaskenv
+++ .flaskenv
@@ -1,8 +1,3 @@
-# this enables debug mode
 FLASK_ENV=development
-
-# the file that's the main entry point for the app
 FLASK_APP=app.py
-
-# port flask opens with
 FLASK_RUN_PORT=3000
--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+from dotenv import load_dotenv
+import random
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home_page():
+    return "🌎 🌍 🌏"
 
-if __name__ == '__main__':
-    app.run()
+# GET /
+@app.route("/greeting")
+def greeting():
+    return render_template("greeting")
+
+
+@app.route('/pie')
+def return_pie_ingredients():
+    ingredients = ['crust','filling','pie pan', 'sugar', 'love', '45 minutes' ,'mama\'s homestyle technique']
+    randIndex = random.randrange(0,len(ingredients),1)
+    return jsonify({'pie ingredient':ingredients[randIndex]})
