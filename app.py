549b129a10e525e3add54110c154d9913c9aa3a6
CHRISDAVIDSPICER <CHRISDAVIDSPICER@GMAIL.COM>
2021-04-01 09:42:19 -0700

    a few additions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, redirect, request
-
+import random
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,15 +15,15 @@ ingredients = ['banana', 'chocolate', 'peanut butter']
 @app.route('/pie', methods=["GET"])
 def ingredients_api():
   global ingredients
-  return jsonify({'pie ingredient': ingredients[0]})
+  rand_idx = random.randint(0, len(ingredients) - 1)
+  return jsonify({'pie ingredient': ingredients[rand_idx]})
 
 @app.route('/recipe', methods=["GET", "POST"])
 def recipe():
   global ingredients
-  if request.method == "POST":
-    ingredient = request.form["ingredient"]
+  if request.method == "GET":
+    return render_template('recipe.html', ingredients=ingredients)
+  elif request.method == "POST":
+    ingredient = request.form['ingredient']
     ingredients.append(ingredient)
-    return jsonify({'pie ingredients': ingredients})
-
-  else:
-    return render_template('recipe.html')
+    return redirect('/recipe')
